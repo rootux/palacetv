5f2a95e4a69ae20b58f504a00883bfb7120c889c
Gal Bracha <galbra@gmail.com>
2019-09-28 16:08:52 -0600

    Fixed https issues

--- mixer.js
+++ mixer.js
@@ -2,21 +2,6 @@ function onYouTubePlayerReady(playerId){
   if (playerId == "ytplayer1"){
     ytplayer1 = document.getElementById("myytplayer1");
     ytplayer1.playVideo();
-  }/* else if(playerId == 'ytplayer2'){
-    ytplayer2 =document.getElementById('myytplayer2');
-    ytplayer2.playVideo();
-    setTimeout(function(){ $('#slider').val(0); slider_change(); }, 500);
-  } else if(playerId == 'ytplayer3'){
-    ytplayer3=document.getElementById('myytplayer3');
-    ytplayer3.playVideo();
-  }*/
-}
-
-function slider_change()
-{
-  var value = $('#slider').val();
-  player.setVolume(100 - value);
-  player2.setVolume(0);
-  player3.setVolume(value - 20);
+  }
 }
 
